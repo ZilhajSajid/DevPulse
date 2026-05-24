9cb918b78b21507d5b1895fc3bc19078fc922d45
Md.Zilhaj Un Noor <zilhajsajid@gmail.com>
2026-05-24 20:36:50 +0600

    hashed password with bcryptjs

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,14 +1,16 @@
 import { pool } from "../../db";
 import type { IUser } from "./user.interface";
-
+import bcrypt from "bcryptjs";
 const createUserIntoDB = async (payload: IUser) => {
   const { name, email, password, role } = payload;
+  const hashPassword = await bcrypt.hashSync(password, 10);
   const result = await pool.query(
     `
     INSERT INTO users (name,email,password,role) VALUES($1,$2,$3,$4)
     RETURNING *`,
-    [name, email, password, role],
+    [name, email, hashPassword, role],
   );
+  delete result.rows[0].password;
   return result;
 };
 
